f59e9dc020b865792c1775ea53997cc3ed0a0f68
Yash Dodani <yashdodani8@gmail.com>
2024-12-11 16:04:13 +0530

    cookie issue 2

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,17 +1,15 @@
-import { useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import Cookies from "js-cookie";
 import React from "react";
 
 function ProtectedRoute({ children }: { children: React.ReactNode }) {
-    const navigate = useNavigate();
     const jwt_token = Cookies.get("jwt");
 
     console.log("Token found " + jwt_token);
 
     if (!jwt_token) {
         console.log("No token found");
-        navigate("/login");
-        return;
+        return <Navigate to="/login" />;
     }
 
     return <>{children}</>;
